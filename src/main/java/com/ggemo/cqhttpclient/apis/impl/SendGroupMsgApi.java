39174a845bbe98de1423f987eb58bdebe5f2666a
HHHHHG <2894700792@qq.com>
2019-11-01 16:21:35 +0800

    setEntity判null

--- src/main/java/com/ggemo/cqhttpclient/apis/impl/SendGroupMsgApi.java
+++ src/main/java/com/ggemo/cqhttpclient/apis/impl/SendGroupMsgApi.java
@@ -4,14 +4,15 @@ import com.ggemo.cqhttpclient.apis.AbstractApi;
 import com.ggemo.cqhttpclient.apis.Api;
 import com.ggemo.cqhttpclient.apis.ApiEnum;
 import com.ggemo.cqhttpclient.requests.HttpPostRequests;
-import com.ggemo.cqhttpclient.vo.request.requestdata.SendGroupMsgRequest;
+import com.ggemo.cqhttpclient.vo.request.impl.SendGroupMsgRequest;
 import com.ggemo.cqhttpclient.vo.response.AbstractResponse;
-import com.ggemo.cqhttpclient.vo.response.response.SendGroupMsgResponse;
+import com.ggemo.cqhttpclient.vo.response.impl.SendGroupMsgResponse;
 import com.ggemo.cqhttpclient.vo.response.responsedata.SendGroupMsgResponseData;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.util.EntityUtils;
 
@@ -28,7 +29,10 @@ public class SendGroupMsgApi extends AbstractApi<SendGroupMsgRequest, AbstractRe
         this.httpPostThreadLocal.set(this.httpPost);
         HttpPost httpPost = this.httpPostThreadLocal.get();
         try {
-            httpPost.setEntity(request.getEntity());
+            UrlEncodedFormEntity entity = request.getEntity();
+            if (entity != null) {
+                httpPost.setEntity(entity);
+            }
             HttpResponse httpResponse = requests.request(httpPost);
             HttpEntity responseEntity = httpResponse.getEntity();
             String responseString = EntityUtils.toString(responseEntity);
